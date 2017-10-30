dfc3228609fdcc4a07c820c7c8f561f7fd5e3d99
CJ Desktop <legatc@rpi.edu>
2017-10-30 13:20:30 -0400

    Created Unity Project & added some rover animations, press space to test them.

---
+++ Discover/Assets/AnimationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool ("Moving", true);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			anim.SetBool ("Moving", false);
+		}
+	}
+}
