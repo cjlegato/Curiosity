71431550314e0bbde646dbc3350eae0a10548b3e
Matthew Castellana <castem4@rpi.edu>
2017-11-07 18:06:00 -0500

    Basic 360 degree camera while debugging y-axis rotation

--- Discover/Assets/Scripts/TP_Camera.cs
+++ Discover/Assets/Scripts/TP_Camera.cs
@@ -4,46 +4,57 @@ using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TP_Camera : MonoBehaviour {
+public class TP_Camera : MonoBehaviour
+{
 
     public Transform target;
 
-	// Use this for initialization
-	void Awake () {
-    Vector3 newPosition = new Vector3(target.position.x, target.position.y + 5, target.position.z + 5);
-    transform.position = newPosition;
-	}
 
-
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(target);
-        if (Input.GetMouseButton(1)) { // if right mouse button is held
-            transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
-            transform.RotateAround(transform.parent.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
-            if (Camera.main.fieldOfView < 100)
-            {
-                Camera.main.fieldOfView += 2;
-            }
-        }
-        else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
-        {
-            if (Camera.main.fieldOfView > 40)
-            {
-                Camera.main.fieldOfView -= 2;
-            }
-        }
-	}
-
-    private Vector3 getDistance()
+    // Use this for initialization
+    void Awake()
     {
-        float x = transform.parent.position.x - transform.position.x;
-        float y = transform.parent.position.y - transform.position.y;
-        float z = transform.parent.position.z - transform.position.z;
-        return new Vector3(x, y, z);
+        Vector3 newPosition = new Vector3(target.position.x, target.position.y + 5, target.position.z + 5);
+        transform.position = newPosition;
+        transform.LookAt(target);
     }
 
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(1)) { // if right mouse button is held
+                                       //transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
+            transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
+            //if (Input.GetAxis("Mouse Y") < 0)
+            //{
+            //    if (transform.localEulerAngles.x < 80)
+            //    {
+            //        transform.RotateAround(transform.parent.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+            //    }
+            //} 
+            //else
+            //{
+            //    if (transform.localEulerAngles.x > 10)
+            //    {
+            //        transform.RotateAround(transform.parent.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+            //    }
+                
+            //}
+            //transform.LookAt(target);
+        }
+         if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+         {
+             if (Camera.main.fieldOfView < 100)
+             {
+                 Camera.main.fieldOfView += 2;
+             }
+         }
+         else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+         {
+             if (Camera.main.fieldOfView > 40)
+             {
+                 Camera.main.fieldOfView -= 2;
+             }
+          }
+    }
 }
