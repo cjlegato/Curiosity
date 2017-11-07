31b29098fdb99c513d88c5db52ed3ce6ca4d7471
Matthew Castellana <castem4@rpi.edu>
2017-11-07 18:54:55 -0500

    Mars with rover and working X-axis camera!

--- Discover/Assets/Scripts/TP_Camera.cs
+++ Discover/Assets/Scripts/TP_Camera.cs
@@ -8,13 +8,14 @@ public class TP_Camera : MonoBehaviour
 {
 
     public Transform target;
-
+    private Vector3 oldTargetPosition;
 
     // Use this for initialization
     void Awake()
     {
-        Vector3 newPosition = new Vector3(target.position.x, target.position.y + 5, target.position.z + 5);
-        transform.position = newPosition;
+        Vector3 oldPosition = new Vector3(target.position.x, target.position.y + 5, target.position.z - 10);
+        transform.position = oldPosition;
+        oldTargetPosition = new Vector3(target.position.x, target.position.y, target.position.z);
         transform.LookAt(target);
     }
 
@@ -22,9 +23,15 @@ public class TP_Camera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+
+        Vector3 newCameraPositionChange = new Vector3(target.position.x - oldTargetPosition.x, target.position.y - oldTargetPosition.y, target.position.z - oldTargetPosition.z);
+        transform.position += newCameraPositionChange;
+        oldTargetPosition = target.position;
         if (Input.GetMouseButton(1)) { // if right mouse button is held
                                        //transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
-            transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
+            //Vector3 newPosition = new Vector3(transform.position.x, target.position.y + 5, transform.position.z);
+            //transform.position = newPosition;
+            transform.RotateAround(target.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
             //if (Input.GetAxis("Mouse Y") < 0)
             //{
             //    if (transform.localEulerAngles.x < 80)
@@ -40,7 +47,7 @@ public class TP_Camera : MonoBehaviour
             //    }
                 
             //}
-            //transform.LookAt(target);
+            transform.LookAt(target);
         }
          if (Input.GetAxis("Mouse ScrollWheel") < 0f)
          {
