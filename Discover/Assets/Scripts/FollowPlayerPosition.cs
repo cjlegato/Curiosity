40b4087ea4a105cc1189adb2f019339550fa14e7
Matthew Castellana <castem4@rpi.edu>
2017-11-08 00:36:05 -0500

    Added collision detection to camera to prevent clipping.

---
+++ Discover/Assets/Scripts/FollowPlayerPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerPosition : MonoBehaviour {
+    public Transform target;
+    private Vector3 oldTargetPosition;
+
+
+    // Use this for initialization
+    void Start () {
+        Vector3 oldPosition = transform.position;
+        oldTargetPosition = target.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newCameraPositionChange = new Vector3(target.position.x - oldTargetPosition.x, target.position.y - oldTargetPosition.y, target.position.z - oldTargetPosition.z);
+        transform.position += newCameraPositionChange;
+        oldTargetPosition = target.position;
+        transform.rotation = target.rotation;
+    }
+}
--- Discover/Assets/Scripts/TP_Camera.cs
+++ Discover/Assets/Scripts/TP_Camera.cs
@@ -7,19 +7,35 @@ using UnityEngine;
 public class TP_Camera : MonoBehaviour
 {
     public Transform target;
+    public float distanceFromTarget;
 
 
     // Use this for initialization
     void Awake()
     {
-        
+        distanceFromTarget = Vector3.Distance(transform.position, target.position);
     }
 
 
     // Update is called once per frame
     void LateUpdate()
     {
+        RaycastHit hit;
+        float distance;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
+        Debug.DrawRay(transform.position, forward, Color.green);
+        if (Physics.Linecast(transform.position, target.position, out hit)) {
+            print("hey");
+        }
+        //if (Physics.Raycast(transform.position, (forward), out hit))
+        //{
+        //    distance = hit.distance;
+        //    print(distance + " " + hit.collider.gameObject.name);
+        //}
+
         // 325 - 20
+        //Debug.Log(transform.localEulerAngles);
         if (transform.localEulerAngles.x > 270) // 325 - 360
         {
             if (Input.GetAxis("Mouse Y") < 0) // going down
@@ -51,5 +67,15 @@ public class TP_Camera : MonoBehaviour
                 }
             }
         }
+        if (checkDistance() != distanceFromTarget)
+        {
+            Vector3.MoveTowards(transform.position, target.position, Mathf.Abs(checkDistance() - distanceFromTarget));
+        }
+        transform.LookAt(target);
+    }
+
+    private float checkDistance()
+    {
+        return Vector3.Distance(transform.position, target.position);
     }
 }
