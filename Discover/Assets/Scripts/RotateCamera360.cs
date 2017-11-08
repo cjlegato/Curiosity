5bc81164bbd109dd6ba0730f2719c49d7eb97a40
Matthew Castellana <castem4@rpi.edu>
2017-11-07 22:11:57 -0500

    Camera works well!

--- Discover/Assets/Scripts/RotateCamera360.cs
+++ Discover/Assets/Scripts/RotateCamera360.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class RotateCamera360 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Transform target;
+    private Vector3 oldTargetPosition;
+    private Quaternion rotation;
+
+    private Vector3 pivotPoint;
+    private Vector3 cameraPoint;
+    private Vector3 direction;
+    public float x = 0;
+    public float y = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        Vector3 oldPosition = new Vector3(target.position.x, target.position.y + 5, target.position.z - 5);
+        transform.position = oldPosition;
+        oldTargetPosition = new Vector3(target.position.x, target.position.y, target.position.z);
+        transform.LookAt(target);
+        rotation = transform.rotation;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(1))
-        { // if right mouse button is held
-          transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
+        Vector3 newCameraPositionChange = new Vector3(target.position.x - oldTargetPosition.x, target.position.y - oldTargetPosition.y, target.position.z - oldTargetPosition.z);
+        transform.position += newCameraPositionChange;
+        oldTargetPosition = target.position;
+
+        transform.RotateAround(target.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
+
+        transform.LookAt(target);   
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (Camera.main.fieldOfView < 100)
+            {
+                Camera.main.fieldOfView += 2;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (Camera.main.fieldOfView > 40)
+            {
+                Camera.main.fieldOfView -= 2;
+            }
         }
     }
 }
--- Discover/Assets/Scripts/TP_Camera.cs
+++ Discover/Assets/Scripts/TP_Camera.cs
@@ -6,62 +6,50 @@ using UnityEngine;
 
 public class TP_Camera : MonoBehaviour
 {
-
     public Transform target;
-    private Vector3 oldTargetPosition;
+
 
     // Use this for initialization
     void Awake()
     {
-        Vector3 oldPosition = new Vector3(target.position.x, target.position.y + 5, target.position.z - 10);
-        transform.position = oldPosition;
-        oldTargetPosition = new Vector3(target.position.x, target.position.y, target.position.z);
-        transform.LookAt(target);
+        
     }
 
 
     // Update is called once per frame
     void LateUpdate()
     {
-
-        Vector3 newCameraPositionChange = new Vector3(target.position.x - oldTargetPosition.x, target.position.y - oldTargetPosition.y, target.position.z - oldTargetPosition.z);
-        transform.position += newCameraPositionChange;
-        oldTargetPosition = target.position;
-        if (Input.GetMouseButton(1)) { // if right mouse button is held
-                                       //transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
-            //Vector3 newPosition = new Vector3(transform.position.x, target.position.y + 5, transform.position.z);
-            //transform.position = newPosition;
-            transform.RotateAround(target.position, new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * 150 * Time.deltaTime);
-            //if (Input.GetAxis("Mouse Y") < 0)
-            //{
-            //    if (transform.localEulerAngles.x < 80)
-            //    {
-            //        transform.RotateAround(transform.parent.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
-            //    }
-            //} 
-            //else
-            //{
-            //    if (transform.localEulerAngles.x > 10)
-            //    {
-            //        transform.RotateAround(transform.parent.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
-            //    }
-                
-            //}
-            transform.LookAt(target);
+        // 325 - 20
+        if (transform.localEulerAngles.x > 270) // 325 - 360
+        {
+            if (Input.GetAxis("Mouse Y") < 0) // going down
+            {
+                //    transform.RotateAround(target.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+                if (transform.localEulerAngles.x > 325)
+                {
+                    transform.RotateAround(target.position, transform.right, Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+                }
+            }
+            else // going up
+            {
+                transform.RotateAround(target.position, transform.right, Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+            }
         }
-         if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-         {
-             if (Camera.main.fieldOfView < 100)
-             {
-                 Camera.main.fieldOfView += 2;
-             }
-         }
-         else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
-         {
-             if (Camera.main.fieldOfView > 40)
-             {
-                 Camera.main.fieldOfView -= 2;
-             }
-          }
+
+        else // 0 - 20
+        {
+            if (Input.GetAxis("Mouse Y") < 0)
+            {
+                transform.RotateAround(target.position, transform.right, Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+            }
+            else // going up, ok to do
+            {
+                //    transform.RotateAround(target.position, new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+                if (transform.localEulerAngles.x < 20)
+                {
+                    transform.RotateAround(target.position, transform.right, Input.GetAxis("Mouse Y") * 150 * Time.deltaTime);
+                }
+            }
+        }
     }
 }
